5d8a589dbbf96b2d7ed839fd829225f15ea49ebc
Hein Htet Aung <heinhtetzaung26698@gmail.com>
2025-05-27 22:18:15 +0630

    page links

--- index.php
+++ index.php
@@ -1,28 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
 
-<head>
-    <title>Document</title>
-</head>
-
-<body>
-    <h1>Recommend Book</h1>
-        <?php 
-            $books = [
-                "Book 1",
-                "Book 2",
-                "Book 3"
-            ]
-        ?>
-
-        <ul>
-            <?php foreach( $books as $book) {
-                echo "<li>$book</li>";
-            }
-
-            ?>
-        </ul>
-    
-</body>
-
-</html>
+require 'index.view.php';
