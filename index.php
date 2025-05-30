85d681ce584460fac845d9f43d9c81724a596ece
Hein Htet Aung <heinhtetzaung26698@gmail.com>
2025-05-30 23:09:53 +0630

    play with database

--- index.php
+++ index.php
@@ -1,4 +1,10 @@
 <?php
 
 require 'functions.php';
-require 'router.php';
+require 'Database.php';
+$config  = require 'config.php';
+
+$db = new Database($config['database']);
+$posts = $db->query("SELECT * FROM posts")->fetchAll(PDO::FETCH_ASSOC);
+
+dd($posts);
